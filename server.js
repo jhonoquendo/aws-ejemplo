38d1283b24dd1229cd55d35becdd80e5b10452a9
Jhon Oquendo <32801020+jhonoquendo@users.noreply.github.com>
2021-03-19 20:40:43 -0500

    Texto modificado

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 
 
-app.get('/',(req,res) => res.send("Hola con express"));
+app.get('/',(req,res) => res.send("<h1>Hola con express</h1>"));
 
 app.listen(3000);
 console.log("Server on port 3000");
